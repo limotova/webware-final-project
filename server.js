663cec5e98e8d4bfe58356c1f16b3d23e60d2659
Larisa Motova <larisa@motovs.org>
2016-12-12 20:13:20 -0500

    added simple searching functions

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ var client = new elasticsearch.Client({
   log: 'trace' // CHANGE THIS TO trace WHEN TESTING/DEBUGGING, info OTHERWISE
 });
 
-
-// THIS IS HOW YOU WOULD PING THE CLIENT
+// this is how you would ping the client
 function clientPing(){
   client.ping({
     requestTimeout: 30000,
@@ -34,24 +33,8 @@ function clientSuperSimpleSearch(){
   });
 }
 
-// IGNORE 404 RESPONSES BY DOING THIS
-function clientDelete(){
-  client.indices.delete({
-    index: 'test_index',
-    ignore: [404]
-  }).then(function (body){
-    // since we told the client to ignore 404 errors, the
-    // promise is resolved even if the index does not exist
-    console.log('index was deleted or never existed');
-  }, function(error){
-    // oh no!
-    console.trace(error.message);
-  });
-}
-
-// THIS IS HOW YOU DO A SLIGHTLY MORE COMPLICATED SEARCH QUERY
 // find songs that have "blues in their title field"
-// function clientSearch(){
+function clientSearch(){
   client.search({
     index: 'million_songs',
     type: 'song',
@@ -64,10 +47,115 @@ function clientDelete(){
     }
   }).then(function (resp){
     var hits = resp.hits.hits;
+    // console.log(resp)
+    console.log(resp.hits.total)  // this is the number of results
   }, function (err){
     console.trace(err.message);
   }); 
-// }
+}
+
+
+
+
+
+
+// SAMPLE FUNCTIONS START HERE:
+
+// NOTE: all min/max years are inclusive (for exclusive do gte-->gt and lte-->lt)
+// for most of these, also consider doing when the year is 0 (that's default/no year)
+function sampleScatterPlotByYears(min_year, max_year){
+  client.search({
+    index: 'million_songs',
+    type: 'song',
+    body: {
+      _source: [
+        "song_hotttnesss",   // categories here
+        "artist_familiarity"    
+        // consider including artist name + song name so we can mouse over things maybe
+      ],
+      size: 10000, 
+      query: {
+        range: {
+          year: {
+            gte: min_year,
+            lte: max_year
+          }
+        }
+      }
+    }
+  }).then(function (resp){
+    console.log(resp.hits.hits) // and then get whatever fields are relevant
+  }, function(err){
+    console.trace(err.message)
+  })
+}
+
+function barGraphByDecades(){
+  client.search({
+    index: 'million_songs',
+    type: 'song',
+    body: {
+      query: {
+        range: {
+          year: {
+            gte: 1920
+          }
+        }
+      },
+      size: 0,
+      aggs: {
+        by_year: {
+          histogram: {
+            field: "year",
+            interval: 10    // 10 years
+          }, 
+          aggs: {
+            avg_hotttnesss: {
+              avg: {
+                field: "song_hotttnesss"
+              }
+            }
+          }
+        }
+      }
+    }
+  }).then(function (resp){
+    console.log(resp)
+  }, function (err){
+    console.trace(err.message);
+  }); 
+}
+
+function topTitleWords(min_year, max_year){
+  client.search({
+    index: 'million_songs', 
+    type: 'song',
+    body: {
+      query: {
+        range: {
+          year: {
+            gte: min_year,
+            lte: max_year
+          }
+        }
+      },
+      size: 0,
+      aggs: {
+        top_title_words: {
+          terms: {
+            field: "title",   // change this to whatever field
+            size: 10          // top 10 most common
+          }
+        }
+      }
+    }
+  }).then(function (resp){
+    console.log(resp.aggregations.top_title_words.buckets)
+    // top words are in key, doc_count == number of times they show up
+  }, function (err){
+    console.trace(err.message);
+  }); 
+}
 
 
 
